37a3b326bb90abbb0efffdd1138c9fcb90102d1b
mittjayswal <mittjayswal@gmail.com>
2017-08-24 16:53:38 -0700

    Book Controller

---
+++ spring-boot-example/src/main/java/com/springboot/bank/springbootexample/BooksController.java
@@ -0,0 +1,19 @@
+package com.springboot.bank.springbootexample;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		return Arrays.asList(
+				  new Book(1l,"Matering in spring","Mitt"));
+		
+	}
+	
+}

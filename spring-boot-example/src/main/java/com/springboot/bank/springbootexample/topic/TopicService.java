02ebb65e53d8e35d484313a8bb251e6c69685d60
mittjayswal <mittjayswal@gmail.com>
2017-08-27 15:43:14 -0700

    Added put post delete

--- spring-boot-example/src/main/java/com/springboot/bank/springbootexample/topic/TopicService.java
+++ spring-boot-example/src/main/java/com/springboot/bank/springbootexample/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.springboot.bank.springbootexample.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private  List<Topic> topics = Arrays.asList(
+	private  List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("1","Hi","SpringBoot"),
-			new Topic("2","Hola","Spanish"));
+			new Topic("2","Hola","Spanish")));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -18,5 +19,22 @@ public class TopicService {
 	
 	public Topic getTopic (String id) {
 		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i =0;i<topics.size();i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t->t.getId().equals(id));
 	}
 }
